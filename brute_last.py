2b3e7b7523ec47d9b98646d575c495349348236b
Axel Alvin <axelalvin1996@gmail.com>
2022-02-02 15:55:01 +0100

    lab solved, next: automate

--- brute_last.py
+++ brute_last.py
@@ -3,7 +3,7 @@ import sys, re, requests
 def listtostring(s):
     return (''.join([str(elem) for elem in s]))
 
-url = "http://dart.cse.kau.se:12345/auth/100/axelalvi/3b1a9f8934eccb4470c9c1b7850d8e"
+url = "http://dart.cse.kau.se:12345/auth/99/axelalvi/798fc0f1716e8ab23cba9273350555"
 
 charlist = ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 #url = 'http://dart.cse.kau.se:12345/auth/100/axelalvi/'
